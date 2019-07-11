da48359d05366fa4e076d51db9dff3bb1470e5cd
Ankita Chatterjee <ankitac3021@gmail.com>
2019-07-11 09:59:26 +0530

    java script practise

--- 3rdclass/challenge.js
+++ 3rdclass/challenge.js
@@ -1,49 +1,54 @@
-/*circular number
-The given number is multiplied by its unit number. This unit number 
-should become the hight of the new number and the rest of elements 
-are same.
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
 
-example: 102564
-             X4
-        ----------
-          410256
-        ----------
-        unit place of orignal number=4
-        high place of new number =4
-        now rest of the numbers of orignal =10256 should be in new number
-        
-
-*/
 var count;
+var c;
+var a;
 var rest1,rest2;
 function func(one,two){
   count=0;
+  
   for(i=0;i<arguments.length;i++)
     {
+      c=0;
+      a=1;
       var num=(arguments[i]);
       var unit=num%10;
       var high=num;
       while(high>=10)
         high=high/10;
       high=parseInt(high);
-      console.log("unit="+unit);
-      console.log("high="+high);
+      //console.log("unit="+unit);
+      //console.log("high="+high);
       
       var newnum=num*unit;
-      console.log("new number="+newnum);
+      //console.log("new number="+newnum);
       
       var newhigh=newnum;
       while(newhigh>=10)
+        {
         newhigh=newhigh/10;
+          c++;
+        }
         
       newhigh=parseInt(newhigh);
        if(newhigh===unit){
-          console.log("matched");
+         // console.log("matched");
          //to compare the rest of the numbers
          rest1=parseInt(num/10);
-         console.log("rest1="+rest1);
-         rest2=parseInt(num/10);
-         console.log("rest2="+rest2);
+        //console.log("rest1="+rest1);
+         while(c>0){
+           a=a*10;
+           c--;
+         }
+        rest2=(newnum-(newhigh*a));
+         //console.log("rest2="+rest2);
          if(rest1===rest2)
            {
              count++;
@@ -59,5 +64,3 @@ func(102564,456);
 console.log("Number of circular numbers among arguments="+count);
 func(102564,11111,345898,12345);
 console.log("Number of circular numbers among arguments="+count);
-
-
